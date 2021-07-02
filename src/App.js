47bd269b3f710368231bb5df7efd80801ab20a23
Gaby Hall <gabyhall@Jonathan-Robertss-iMac.local>
2021-07-02 14:52:32 +0100

    calculator 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  const [num, setNum] = useState("");
+  
+  const handleClick = (e) => {
+    setNum(num.concat(e.target.name));
+  }
+
+  const calc = () => {
+    setNum(eval(num).toString());
+  }
+
+return (
+  <div>
+      <h1>{num}</h1>
+      <div className="buttons">
+          <div className="row1">
+            <button onClick={handleClick} name="/">/</button>
+            <button onClick={handleClick} name="*">x</button>
+            <button onClick={handleClick} name="+">+</button>
+            <button onClick={handleClick} name="-">-</button>
+          </div>
+          <div className="row2">
+            <button onClick={() => setNum("")}>C</button>
+            <button id="equals" onClick={calc}>=</button>
+            
+          </div>
+          <div className="row3">
+            <button onClick={handleClick} name="7">7</button>
+            <button onClick={handleClick} name="8">8</button>
+            <button onClick={handleClick} name="9">9</button>
+          </div>
+          <div className="row4">
+            <button onClick={handleClick} name="4">4</button>
+            <button onClick={handleClick} name="5">5</button>
+            <button onClick={handleClick} name="6">6</button>
+               <button onClick={handleClick} name=".">.</button>
+          </div>
+          <div className="row5">
+            <button onClick={handleClick} name="1">1</button>
+            <button onClick={handleClick} name="2">2</button>
+            <button onClick={handleClick} name="3">3</button>
+
+            <button onClick={handleClick} name="0">0</button>
+          </div>
+
+      </div>
+  </div>
+  )
 }
+
 
 export default App;
